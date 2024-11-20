cb8144449c8355d48db505f4331ab94e618a8e69
CMartinez <charli.rey.misterio@gmail.com>
2024-11-19 19:02:53 -0600

    fixed

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-# Ejecutar migraciones
-php artisan migrate --force
+# Ejecutar migraciones y capturar errores
+if ! php artisan migrate --force; then
+    echo "Error: Las migraciones no se ejecutaron correctamente."
+    exit 1
+fi
 
 # Iniciar PHP-FPM
 php-fpm
